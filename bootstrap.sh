4c6f78ba0f79ea822667e7dea168b5b413f3a7bd
enginearn <enginyaaaaan@gmail.com>
2022-05-12 15:09:01 +0900

    bash script変更

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt update
-sudo apt install -y apache2
+sudo apt-get update && upgrade -y
+sudo apt-get install -y apache2
 if ! [ -L /var/www ]; then
   sudo rm -rf /var/www
   sudo ln -fs /vagrant /var/www
